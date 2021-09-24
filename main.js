997607ccc10ecefaff24eb631340f814543a8ee9
Syedkn <77274689+Syedkn@users.noreply.github.com>
2021-09-24 16:48:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+video="";
+status="";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    ObjectDetector = ml5.ObjectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
